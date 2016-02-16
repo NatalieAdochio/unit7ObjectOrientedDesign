876f50cdb30c023c68f907be4fb0891ef363bd07
NatalieAdochio <nhadochio@NN123-HA-23.HS.sd203.org>
2016-02-16 14:12:41 -0600

    2/16

--- Triangle/Triangle.java
+++ Triangle/Triangle.java
@@ -22,6 +22,8 @@ public class Triangle
     private int clicks;
 
     private JFrame frame;
+    private static final int FRAMEW=100;
+    private static final int FRAMEH = 100;
     /**
      * Default constructor for objects of class Triangle
      */
@@ -36,25 +38,78 @@ public class Triangle
         //         
         clicks = 0;
 
-        MouseListener listener = new MouseListener();
+        
+        MouseListener listener = new MousePressListener();
+
+        this.frame.setSize(FRAMEW,FRAMEH);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setVisible(true);
     }
 
-    public class MouseListener implements ActionListener
+    public class MousePressListener implements MouseListener
     {
         // put your code here
-        public void actionPerformed(ActionEvent event)
+        public void mouseClicked(MouseEvent event)
         {
             double dotLocX = this.getX();
             double dotLocY = this.getY();
-            Point2D elli = new Point2D();
-            elli.setLocation(dotLocX, dotLocY);
+            //Point2D second = new Point2D();
+            //second.setLocation(dotLocX, dotLocY);
             clicks++;
-            if(clicks ==2)
+            if (clicks ==1)
             {
-                Line2D line = new Line2D;
-                line.setLine(elli, 
+                Point2D elli = new Point2D();
+                elli.setLocation(dotLocX, dotLocY);
+            }
+            else if(clicks ==2)
+            {
+                Line2D secondLine = new Line2D();
+                Point2D second = new Point2D();
+                second.setLocation(dotLocX, dotLocY);
+                secondLine.setLine(elli, second); 
+                Graphics2D g2 = (Graphics2D) g;
+                g2.draw(SecondLine);
+            }
+            else if(clicks ==3)
+            {
+                Line2D thirdLine = new Line2D();
+                Point2D third = new Point2D();
+                third.setLocation(dotLocX, dotLocY);
+                thirdLine.setLine(elli, third);
+                Graphics2D g4 = (Graphics2D) g;
+                g4.draw(thirdLine);
+
+                Line2D lastLine = new Line2D();
+                LastLine.setLine(third, second);
+                Graphics2D g3 = (Graphics2D) g;
+                g3.draw(LastLine);
+            }
+            else if(clicks ==4)
+            {
+                clicks = 0;
+                repaint();
             }
         }
-    }
 
+        public void mouseReleased(MouseEvent event)
+        {
+
+        }  
+
+        public void mouseEntered(MouseEvent event)
+        {
+
+        }
+
+        public void mouseExited(MouseEvent event)
+        {
+
+        }
+        public void mousePressed(MouseEvent event)
+            {
+                
+            }
+    }
 }
+
+
